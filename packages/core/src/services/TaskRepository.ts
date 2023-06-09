632e72b081e16cee3b269ba49c8510cfe76e745e
reifujimura <reifujimura@outlook.com>
2023-06-09 09:27:56 +0000

    Add arguments.

--- packages/core/src/services/TaskRepository.ts
+++ packages/core/src/services/TaskRepository.ts
@@ -1,9 +1,9 @@
-import { Adapters } from "../adapters";
+import { Adapters, Query } from "../adapters";
 import { Task } from "../domain";
 
 export class TaskRepository {
   constructor(private readonly adapters: Adapters) {}
-  async get(): Promise<Task[]> {
+  async get(query?: Query<Task>): Promise<Task[]> {
     return this.adapters.gateways.task.get();
   }
 }
